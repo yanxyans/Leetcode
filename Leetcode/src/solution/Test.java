e13f1ae134e3f72f17d3518ebea256198b7fcf9d
Jason Yan <XBBNC8U@ams.bnymellon.net>
2018-01-29 11:13:38 -0500

    is power of two

--- Leetcode/src/solution/Test.java
+++ Leetcode/src/solution/Test.java
@@ -4,10 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		String[] data = new String[] {"5","2","C","D","+"};
-		BaseballGame test = new BaseballGame();
-		int ret = test.calPoints(data);
-		System.out.println(ret);
+		int test = 0x80000000;
+		IsPowerOfTwo isPowerOfTwo = new IsPowerOfTwo();
+		System.out.println(isPowerOfTwo.isPowerOfTwo(test));
 	}
 
 }
