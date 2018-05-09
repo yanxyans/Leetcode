041361ffbd5a5e1a02d66b02c15a01a630a1403d
Jason Yan <XBBNC8U@ams.bnymellon.net>
2018-05-09 14:41:19 -0400

    three sum with hash table

--- Leetcode/src/solution/Test.java
+++ Leetcode/src/solution/Test.java
@@ -1,18 +1,33 @@
 package solution;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
 public class Test {
 
 	public static void main(String[] args) {
 		
-		QuickSort quickSort = new QuickSort();
-		int[] data = new int[] {1, 3, 4, 5, 1, 2, 0, 5, 11, 3};
-		
-		System.out.println("before sort: " + Arrays.toString(data));
-		quickSort.sort(data);
-		System.out.println("after sort " + Arrays.toString(data));
+		ThreeSum threeSum = new ThreeSum();
+		int[] nums = new int[] {-1, 0, 1, 2, -1, -4};
+		test(threeSum, nums);
+		int[] nums1 = new int[] {-1, -1, 0, 0, 1, 1, -2, 2};
+		test(threeSum, nums1);
+		int[] nums2 = new int[] {-2, -2, -1, 0, 0, 1, 2, 2, 3, 4};
+		test(threeSum, nums2);
 	}
 
+	public static void test(ThreeSum threeSum, int[] nums) {
+		System.out.println("test for: " + Arrays.toString(nums));
+		List<List<Integer>> ret = threeSum.threeSum(nums);
+		HashSet<String> uniq = new HashSet<String>();
+		for (List<Integer> r : ret) {
+			Collections.sort(r);
+			System.out.println(r.toString());
+			uniq.add(r.toString());
+		}
+		System.out.println("total elements: " + ret.size() + ", unique elements: " + uniq.size());
+	}
 }
---
+++ Leetcode/src/solution/ThreeSum.java
@@ -0,0 +1,30 @@
+package solution;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        ArrayList<List<Integer>> trip = new ArrayList<List<Integer>>();
+        HashSet<Integer> seen = new HashSet<Integer>();
+        
+        Arrays.sort(nums);
+        
+        for (int i = 0; i < nums.length; i++) {
+        	int target = nums[i];
+        	for (int j = i + 1; j < nums.length; j++) {
+        		if (seen.contains(-target -nums[j])) {
+        			trip.add(Arrays.asList(target, nums[j], -target -nums[j]));
+        		}
+        		seen.add(nums[j]);
+        		while (j < nums.length - 1 && nums[j] == nums[j + 1]) j++;
+        	}
+        	seen.clear();
+        	while (i < nums.length - 1 && nums[i] == nums[i + 1]) i++;
+        }
+        
+        return trip;
+    }
+}
